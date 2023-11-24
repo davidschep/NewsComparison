8679f1208c41cf955c7e528ee180491a3aa4e04f
Magnus Jensen <s184677@dtu.dk>
2023-11-24 21:55:29 +0100

    kmeans and elbow

--- news_connector.py
+++ news_connector.py
@@ -4,79 +4,23 @@
 #
 ###########################
 
-
-import math
-import numpy as np
-import pandas as pd
 import pandas as pd
 import numpy as np
-import collections
-from numpy import linalg
 from collections import Counter
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
 import re
-import os
 import warnings
+from matplotlib import cm
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
 warnings.filterwarnings("ignore")
-
-
-def filter_common_words(df, threshold=0.95):
-    # Count the frequency of each word in the dataframe
-    word_counts = Counter()
-    for idx, row in df.iterrows():
-        words = row['content'].split()
-        word_counts.update(words)
-
-    # Determine the threshold for word frequency
-    N = len(df)
-    threshold = threshold * N
-
-    # Filter out words that exceed the threshold
-    common_words = []
-    for word, count in word_counts.items():
-      if count <= threshold:
-        common_words.append(word)
-
-    # Apply the filter function to each row of the "content" column
-    def filter_row(row):
-      words = row.split()
-      filtered_words = []
-      for word in words:
-          if word in common_words:
-              filtered_words.append(word)
-      return ' '.join(filtered_words)
-
-    df['content'] = df['content'].apply(filter_row)
-
-    return df
-
-def remove_infrequent_words(df, min_frequency=5):
-    # Combine all strings in the specified column into a single string
-    text = ' '.join(df['content'])
-
-    # Tokenize the combined string into words
-    words = text.split()
-
-    # Count the frequency of each word using Counter
-    word_counts = Counter(words)
-
-    # Identify words that occur less than min_frequency times
-    infrequent_words = []
-    for word, count in word_counts.items():
-       if count < min_frequency:
-        infrequent_words.append(word)
-
-    # Function to remove infrequent words from a text
-    def remove_infrequent(text):
-        return ' '.join([word for word in text.split() if word not in infrequent_words])
-
-    # Apply the remove_infrequent function to the specified column
-    df['content'] = df['content'].apply(remove_infrequent)
-
-    return df
+from IPython.display import clear_output
 
 # Function to preprocess content column
 def preprocess_text(text):
@@ -139,9 +83,6 @@ def extract_documents(df):
     Returns:
         tf idf matrix
     """
-    
-    df = filter_common_words(df)
-    
     # Download NLTK resources
     nltk.download('stopwords')
     
@@ -160,9 +101,104 @@ def extract_documents(df):
     tfidf_matrix = tf_idf(tf_matrix, idf, docs)
     return tfidf_matrix
 
+class KMeansAlgorithm:
+
+    def __init__(self, X, k, max_iterations=100):
+      self.X = X.values
+      self.k = k
+      self.max_iterations = max_iterations
+      self.centroids = None
+      self.num_articles = X.shape[0]
+
+    def init_random_centroids(self):
+      # Initializing KMeans by choosing random centroids 
+      idx = np.random.choice(self.num_articles, size=self.k, replace=False) # Extract random indices from dataframe
+      centroids = self.X[idx] # Pick random rows from the dataframe
+      return centroids
+
+    def calculate_euclidean_distances(self):
+      # Calculate distances from vector/row to centroid
+      num_centroids = self.centroids.shape[0]
+      distances = np.zeros((num_centroids, self.num_articles))
+
+      for centroid_idx in range(num_centroids):
+          for article_idx in range(self.num_articles):
+              distances[centroid_idx, article_idx] = np.sqrt(np.sum((self.centroids[centroid_idx, :] - self.X[article_idx, :]) ** 2))
+      return distances
+
+    def update_centroids(self, labels):
+        # Calculate the mean of each cluster as new centroid
+        new_centroids = []
+        for k in range(self.k):
+            mean = self.X[labels == k].mean(axis=0)
+            new_centroids.append(mean)
+        new_centroids = np.array(new_centroids)
+        return new_centroids
+
+    def plot_clusters(self, labels, centroids, iteration):
+        # We will use PCA to plots clusters as the TFIDF matrix has many dimensions
+        unique_labels = np.unique(labels)
+        pca = PCA(n_components=3)
+        data_3d = pca.fit_transform(self.X)
+        centroids_3d = pca.transform(centroids)
+        # 3D plot
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection='3d')
+        color_map = cm.get_cmap('turbo', len(unique_labels))
+        # Plot data
+        for label in unique_labels:
+            indices = labels == label # like [1, 2, 1, 3, 1]
+            ax.scatter(data_3d[indices, 0], data_3d[indices, 1], data_3d[indices, 2], label=f'Cluster {label}', c=[color_map(label)])
+        # Plot centroids
+        ax.scatter(centroids_3d[:, 0], centroids_3d[:, 1], centroids_3d[:, 2], c='red', marker='x', s=80, label='Centroids')
+        ax.set_title(f'PCA 3D - iteration {iteration}')
+        ax.set_xlabel('Principal component 1')
+        ax.set_ylabel('Principal component 2')
+        ax.set_zlabel('Principal component 3')
+        ax.legend(loc='center left', bbox_to_anchor=(1.1, 0.5))
+        clear_output(wait=True)
+        plt.show()
+
+    def fit(self):
+      
+      # Run the Kmeans algorithm using helper functions
+      self.centroids = self.init_random_centroids() # Init centroids
+
+      for i in range(self.max_iterations):
+        distances = self.calculate_euclidean_distances() # calculate eucledian distances
+        labels = np.argmin(distances, axis=0) # Assign to the cluster with the centroid that has the minimum distance to that point
+        new_centroids = self.update_centroids(labels) # Calculated centroids based on mean of the points in that cluster
+
+        if np.all(new_centroids == self.centroids): # If no new centroids break loop
+          print("Kmeans has converged!")
+          break
+
+        self.centroids = new_centroids
+        self.plot_clusters(labels, self.centroids, i) # Plot PCA 3D plot
+
+      return labels
+
 def KMeans(k, df):
-    kmeans = KMeans(k, random_state=123)
     tfidf_matrix = extract_documents(df)
-    kmeans.fit(tfidf_matrix)
-    df['cluster'] = kmeans.labels_
+    Kmeans = KMeansAlgorithm(tfidf_matrix, k) # Optimal number of clusters is determined from the results of the elbow method
+    labels = Kmeans.fit()
+    df['cluster'] = labels
     return df
+
+def elbow_method(data):
+    # We will use elbow method to determine optimal number of clusters
+    num_clusters = range(1, 30)
+    wcss = []
+
+    for k in num_clusters:
+        # We will use Sklearn's Kmeans algorithm, but use random intialization as used in our implementation
+        kmeans = KMeans(n_clusters=k, init='random', random_state=123)
+        kmeans.fit(data)
+        wcss.append(kmeans.inertia_)
+
+    # Plot the Elbow Method
+    plt.plot(num_clusters, wcss, marker='o')
+    plt.title('Elbow Method - KMeans with random initialization')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Wcss')
+    plt.show()
